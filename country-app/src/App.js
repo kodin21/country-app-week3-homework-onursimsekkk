4a638008fc9f46bffcda35951d2c79e49f9eee98
Onur Simsek <onur.simsekk3@gmail.com>
2021-07-24 01:01:45 +0300

    Add CountryCard component to App

--- country-app/src/App.js
+++ country-app/src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
 import './App.css';
+// Components
+import CountryCard from './components/CountryCard';
+
 
 function App() {
+
   return (
     <div className="App">
-      sa
+      <CountryCard />
     </div>
   );
 }
