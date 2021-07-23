63d1367a3f8d234ea63bb166cde1aea315b5b5eb
Onur Simsek <onur.simsekk3@gmail.com>
2021-07-24 01:21:31 +0300

    Add Selection component

---
+++ country-app/src/components/Selection.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Selection = () => {
+
+  return (
+    <div>
+      <button className="selection-btn">Countries</button>
+      <button className="selection-btn">Statistics</button>
+    </div>
+  )
+  
+};
+
+export default Selection;
