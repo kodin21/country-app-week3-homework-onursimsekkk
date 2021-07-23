89f117ca25be2741aa8dcb47ee58b11b0ea2cc86
Onur Simsek <onur.simsekk3@gmail.com>
2021-07-24 01:22:39 +0300

    Add Selection comp. to Header

---
+++ country-app/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Selection from './Selection';
+
+const Header = () => {
+
+  return (
+    <div>
+      <h1>
+        Country App
+      </h1>
+      <div>
+        <Selection />
+      </div>
+    </div>
+  )
+ 
+};
+
+export default Header;
