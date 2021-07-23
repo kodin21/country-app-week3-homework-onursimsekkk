685b50be5c9daa2361fec06930d178d9fa9b7e25
Onur Simsek <onur.simsekk3@gmail.com>
2021-07-24 01:22:16 +0300

    Add Header component

--- country-app/src/App.js
+++ country-app/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 // Components
 import CountryCard from './components/CountryCard';
+import Header from './components/Header';
 
 
 function App() {
 
   return (
     <div className="App">
+      <Header />
       <CountryCard />
     </div>
   );
