02f2e7f80ca051a1e91bb80dd39775f3a72ade6d
Onur Simsek <onur.simsekk3@gmail.com>
2021-07-24 21:39:06 +0300

    Add props

--- country-app/src/components/Header.js
+++ country-app/src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Selection from './Selection';
 
-const Header = () => {
+const Header = (props) => {
 
   return (
     <div>
       <h1>
-        Country App
+        {props.title}
       </h1>
       <div>
         <Selection className="mt-5" />
