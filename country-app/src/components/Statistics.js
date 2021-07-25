de24ba419e32e44f37db67839bc0fb150297fe6d
Onur Simsek <onur.simsekk3@gmail.com>
2021-07-25 23:43:21 +0300

    Edit card style

--- country-app/src/components/Statistics.js
+++ country-app/src/components/Statistics.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 
 const Statistics = () => {
+  const [countries, setCountries] = useState([]);
 // Send a GET request with Axios
 useEffect(() => {
   axios.get('https://restcountries.eu/rest/v2/all')
@@ -8,16 +11,23 @@ useEffect(() => {
 }, []);
 
 return (
-  <Card border="info" style={{ width: '18rem' }}>
-    <Card.Header>Header</Card.Header>
-    <Card.Body>
-      <Card.Title>Info Card Title</Card.Title>
-      <Card.Text>
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
-      </Card.Text>
-    </Card.Body>
-  </Card>
+  <Container className="mt-4">
+    <Row className="justify-content-md-center mb-3">
+    {countries.map(country => {
+      <Card border="info" style={{ width: '18rem' }}>
+          <Card.Header>{country.language}</Card.Header>
+          <Card.Body>
+            <Card.Title>Info Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk
+              of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+    })}
+    </Row>
+  </Container>  
+  
 )
 };
 
