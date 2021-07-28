cb842da5dc97a2d058d2654084b41b1f58466eb5
Onur Simsek <onur.simsekk3@gmail.com>
2021-07-29 00:32:03 +0300

    Add basic view to component

--- country-app/src/components/Statistics.js
+++ country-app/src/components/Statistics.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Row from 'react-bootstrap/Row'
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
 
 
 const Statistics = () => {
@@ -10,20 +16,26 @@ useEffect(() => {
   .then(response => setCountries(response.data));
 }, []);
 
+const statistic = countries.map
+
 return (
   <Container className="mt-4">
-    <Row className="justify-content-md-center mb-3">
+    <Row className="justify-content-center mb-3">
     {countries.map(country => {
-      <Card border="info" style={{ width: '18rem' }}>
-          <Card.Header>{country.language}</Card.Header>
-          <Card.Body>
-            <Card.Title>Info Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk
-              of the card's content.
-            </Card.Text>
-          </Card.Body>
-        </Card>
+      return (
+        <Col className="col-12 mb-4" key={country.name}>
+          <Card border="info" style={{ width: '32rem' }}>
+            <Card.Header>{country.name}</Card.Header>
+            <Card.Body>
+              <Card.Title>Info Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk
+                of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      )
     })}
     </Row>
   </Container>  
