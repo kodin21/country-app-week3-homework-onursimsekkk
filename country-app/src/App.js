11974fc6c30c09c558d94251c000074023802ea6
Onur Simsek <onur.simsekk3@gmail.com>
2021-07-29 00:32:44 +0300

    Add setAppPage func

--- country-app/src/App.js
+++ country-app/src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 // Components
+import Button from 'react-bootstrap/Button';
 import CountryCard from './components/CountryCard';
 import Header from './components/Header';
+import Statistics from './components/Statistics';
+
 
 
 function App() {
+const [appPage, setAppPage] = useState(true);
 
   return (
     <div className="App">
       <Header title="Country App" />
+      <Button onClick={() => setAppPage(true)} variant={appPage ? "primary" : "outline-primary"} className="btn-lg">Countries </Button>
+      <Button onClick={() => setAppPage(false)} variant={appPage ? "outline-primary" : "primary"} className="btn-lg">Statistics </Button>
+      {appPage ? (
       <CountryCard />
-         
-    </div>
+      ) : (
+      <Statistics />
+      ) }  
+      
+      
+    </div> 
   );
 }
 
